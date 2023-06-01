11e4caea064ec93652e1559f5e1bc74ac7ec667f
Guilherme Defavori Fabretti <guidfabretti@hotmail.com>
2023-06-01 14:21:31 -0300

    Animações topzeras

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ function menuAparecer(){
     let menuMobile = document.querySelector('.menu-mobile')
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = "img/menu-de-hamburguer.png"
     }else{
         menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "img/fechar.png"
     }
 }
