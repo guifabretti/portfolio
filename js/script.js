6fab28937091dde986819fb9f1f12d40d84c8ae8
Guilherme Defavori Fabretti <guidfabretti@hotmail.com>
2023-06-02 18:02:43 -0300

    add algumas coisas no main

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ function menuAparecer(){
         menuMobile.classList.add('open')
     }
 }
+
+
+function changeImage(element) {
+    element.setAttribute('data-original-src', element.src);
+
+    if (element.alt === 'HTML5') {
+        element.src = '/img/icones-code-hover/html-5.png';
+    } else if (element.alt === 'CSS3') {
+        element.src = 'img/icones-code-hover/css-3.png';
+    } else if (element.alt === 'JavaScript') {
+        element.src = 'img/icones-code-hover/js.png';
+    } else if (element.alt === 'Git') {
+        element.src = 'img/icones-code-hover/git.png';
+    } else if (element.alt === 'Figma') {
+        element.src = 'img/icones-code-hover/figma.png';
+    }
+}
+
+function restoreImage(element) {
+    // Restaura a imagem original usando o atributo personalizado
+    const originalSrc = element.getAttribute('data-original-src');
+    element.src = originalSrc;
+}
